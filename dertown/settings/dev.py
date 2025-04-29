66a69ea711756ebe822e6da164877e86d173796b
Elizabeth Sall <easall@gmail.com>
2025-04-29 11:32:30 -0700

    Initial commit: Clean Wagtail setup with single templates dir, Tailwind, and event model.

---
+++ dertown/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*sim25@_u4)!vi$g*6d5^rh1o%&7hdrqrg+@y31-=r!lyppy=j"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
