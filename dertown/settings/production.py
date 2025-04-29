f9b07a57eb4a4f6c4ad4797935870dde84dc0dd3
Elizabeth Sall <easall@gmail.com>
2025-04-29 12:25:22 -0700

    Prepare for Render deployment

--- dertown/settings/production.py
+++ dertown/settings/production.py
@@ -1,8 +1,16 @@
 from .base import *
 
 DEBUG = False
+ALLOWED_HOSTS = ['.onrender.com']
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# Set SECRET_KEY from environment variable
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default')
+
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
