d5938fe4981b640cbd4a1fc6ff85583cbcc2618e
Elizabeth Sall <easall@gmail.com>
2025-04-29 13:25:46 -0700

    Add debug settings for troubleshooting

---
+++ dertown/settings/debug.py
@@ -0,0 +1,26 @@
+from .production import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+# Enable detailed error reporting
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+} 
