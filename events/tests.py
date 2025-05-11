99424f404d58b376e50f4007a7f6ff0c5fb80a23
Elizabeth Sall <easall@gmail.com>
2025-05-11 16:15:46 -0700

    merge migrations for real

--- events/tests.py
+++ events/tests.py
@@ -1,1 +1,9 @@
 # Create your tests here.
+
+from django.test import TestCase
+
+
+class MigrationSmokeTest(TestCase):
+    def test_migrations_apply(self):
+        # This test will always pass, but ensures migrations are applied
+        self.assertTrue(True)
